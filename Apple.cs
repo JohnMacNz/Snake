2710c6c471dc28d27be3a28d57949f0acc9ce60b
JohnMacNz <john.mclaren013@hotmail.com>
2016-05-19 01:06:05 +1200

    Update Apple.cs

--- Apple.cs
+++ Apple.cs
@@ -1,4 +1,4 @@
-﻿/* Author: John R. McLaren
+/* Author: John R. McLaren
  * Created: 6/5/2016
  * Rev: 1.0.0
  * 
@@ -15,13 +15,13 @@ namespace Snake
     class Apple
     {
         Random rng = new Random();
-        Grid grid = new Grid();
-        Shape shapes = new Shape();
+        Grid Grid = new Grid();
+        Shape Shape = new Shape();
 
         // apple rendering logic, assigns new location for apple then draws it
-        public void GenerateApple(List<Point> appleCoords, List<Point> snakeCoords, PictureBox picBox)
+        public void Generate(List<Point> appleCoords, List<Point> snakeCoords, PictureBox picBox)
         {
-            appleCoords.Add(new Point(rng.Next(1, grid.GridSize) * grid.SquareSize, rng.Next(1, grid.GridSize) * grid.SquareSize)); // new apple location
+            appleCoords.Add(new Point(rng.Next(1, Grid.GridSize) * Grid.SquareSize, rng.Next(1, Grid.GridSize) * Grid.SquareSize)); // new apple location
 
             for (int i = 0; i < snakeCoords.Count - 1; i++) // loop for each segment in snake
             {
@@ -33,16 +33,16 @@ namespace Snake
             }
             if (appleCoords.Count > 0) // if apple exists
             {
-                shapes.DrawSolidCircle(picBox, appleCoords[appleCoords.Count - 1].X + 3, appleCoords[appleCoords.Count - 1].Y + 3, 10, Color.Red);
+                Shape.DrawSolidCircle(picBox, appleCoords[appleCoords.Count - 1].X + 3, appleCoords[appleCoords.Count - 1].Y + 3, 10, Color.Red);
             }
             else
             {
-                GenerateApple(appleCoords, snakeCoords, picBox); // try again
+                Generate(appleCoords, snakeCoords, picBox); // try again
             }
         }
 
         // checks apple collision
-        public bool HitApple(int hitX, int hitY, List<Point> appleCoords)
+        public bool Hit(int hitX, int hitY, List<Point> appleCoords)
         {
             if (appleCoords[appleCoords.Count - 1].X == hitX && appleCoords[appleCoords.Count - 1].Y == hitY) // if apple coordinate same as hitX+hitY
             {
