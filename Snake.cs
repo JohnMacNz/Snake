b594759fcf5d85cbf0d71705485336b0a14bca68
JohnMacNz <john.mclaren013@hotmail.com>
2016-05-19 01:06:38 +1200

    Update Snake.cs

--- Snake.cs
+++ Snake.cs
@@ -1,4 +1,4 @@
-﻿/* Author: John R. McLaren
+/* Author: John R. McLaren
  * Created: 6/5/2016
  * Rev: 1.0.0
  * 
@@ -14,38 +14,37 @@ namespace Snake
 {
     class Snake
     {
-        Shape shapes = new Shape();
-        Grid grid = new Grid();
-        Apple apple = new Apple();
+        Shape Shape = new Shape();
+        Grid Grid = new Grid();
 
-        // adds new segment to list
-        public void GrowSnake(string direction, PictureBox pictureBox, List<Point> list, Color rgb)
+        // adds new segment to snake
+        public void Grow(string direction, PictureBox pictureBox, List<Point> list, Color rgb)
         {
             Point coordinates = list[list.Count - 1]; // last/bottom of the list
 
             switch (direction)
             {
                 case "up":
-                    list.Add(new Point(coordinates.X, coordinates.Y - grid.SquareSize)); // new head pos
-                    shapes.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, grid.SquareSize, rgb); // draw head
+                    list.Add(new Point(coordinates.X, coordinates.Y - Grid.SquareSize)); // new head pos
+                    Shape.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, Grid.SquareSize, rgb); // draw head
                     break;
                 case "down":
-                    list.Add(new Point(coordinates.X, coordinates.Y + grid.SquareSize));
-                    shapes.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, grid.SquareSize, rgb);
+                    list.Add(new Point(coordinates.X, coordinates.Y + Grid.SquareSize));
+                    Shape.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, Grid.SquareSize, rgb);
                     break;
                 case "left":
-                    list.Add(new Point(coordinates.X - grid.SquareSize, coordinates.Y));
-                    shapes.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, grid.SquareSize, rgb);
+                    list.Add(new Point(coordinates.X - Grid.SquareSize, coordinates.Y));
+                    Shape.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, Grid.SquareSize, rgb);
                     break;
                 case "right":
-                    list.Add(new Point(coordinates.X + grid.SquareSize, coordinates.Y));
-                    shapes.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, grid.SquareSize, rgb);
+                    list.Add(new Point(coordinates.X + Grid.SquareSize, coordinates.Y));
+                    Shape.DrawSolidSquare(pictureBox, coordinates.X, coordinates.Y, Grid.SquareSize, rgb);
                     break;
             }
         }
 
-        // removes tail position and redraws snake
-        public void MoveSnake(string direction, PictureBox pictureBox, List<Point> snakeCoords, Color rgb)
+        // removes tail and redraws snake at new location
+        public void Move(string direction, PictureBox pictureBox, List<Point> snakeCoords, Color rgb)
         {
             Point lastPoint = snakeCoords[snakeCoords.Count - 1]; // last item on list list
             Point firstPoint = snakeCoords[0]; // first item on list list
@@ -53,27 +52,27 @@ namespace Snake
             switch (direction)
             {
                 case "up":
-                    snakeCoords.Add(new Point(lastPoint.X, lastPoint.Y - grid.SquareSize)); // new head pos
-                    shapes.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, grid.SquareSize, Color.FromArgb(32, 32, 32)); // erase tail
-                    shapes.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, grid.SquareSize, rgb);
+                    snakeCoords.Add(new Point(lastPoint.X, lastPoint.Y - Grid.SquareSize)); // new head pos
+                    Shape.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, Grid.SquareSize, Color.FromArgb(32, 32, 32)); // erase tail
+                    Shape.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, Grid.SquareSize, rgb);
                     snakeCoords.RemoveAt(0); // remove tail pos
                     break;
                 case "down":
-                    snakeCoords.Add(new Point(lastPoint.X, lastPoint.Y + grid.SquareSize));
-                    shapes.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, grid.SquareSize, Color.FromArgb(32, 32, 32));
-                    shapes.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, grid.SquareSize, rgb);
+                    snakeCoords.Add(new Point(lastPoint.X, lastPoint.Y + Grid.SquareSize));
+                    Shape.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, Grid.SquareSize, Color.FromArgb(32, 32, 32));
+                    Shape.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, Grid.SquareSize, rgb);
                     snakeCoords.RemoveAt(0);
                     break;
                 case "left":
-                    snakeCoords.Add(new Point(lastPoint.X - grid.SquareSize, lastPoint.Y));
-                    shapes.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, grid.SquareSize, Color.FromArgb(32, 32, 32));
-                    shapes.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, grid.SquareSize, rgb);
+                    snakeCoords.Add(new Point(lastPoint.X - Grid.SquareSize, lastPoint.Y));
+                    Shape.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, Grid.SquareSize, Color.FromArgb(32, 32, 32));
+                    Shape.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, Grid.SquareSize, rgb);
                     snakeCoords.RemoveAt(0);
                     break;
                 case "right":
-                    snakeCoords.Add(new Point(lastPoint.X + grid.SquareSize, lastPoint.Y));
-                    shapes.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, grid.SquareSize, Color.FromArgb(32, 32, 32));
-                    shapes.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, grid.SquareSize, rgb);
+                    snakeCoords.Add(new Point(lastPoint.X + Grid.SquareSize, lastPoint.Y));
+                    Shape.DrawSolidSquare(pictureBox, firstPoint.X, firstPoint.Y, Grid.SquareSize, Color.FromArgb(32, 32, 32));
+                    Shape.DrawSolidSquare(pictureBox, lastPoint.X, lastPoint.Y, Grid.SquareSize, rgb);
                     snakeCoords.RemoveAt(0);
                     break;
             }
